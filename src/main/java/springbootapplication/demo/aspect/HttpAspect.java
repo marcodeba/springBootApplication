e66287a5980dffe95223a1661b25220d93ed86d6
panning255 <panning255@jk.cn>
2019-03-21 23:28:34 +0800

    add aspect aop

---
+++ src/main/java/springbootapplication/demo/aspect/HttpAspect.java
@@ -0,0 +1,48 @@
+package springbootapplication.demo.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import springbootapplication.demo.dao.entity.Student;
+import springbootapplication.demo.domain.Result;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Aspect
+@Component
+@Slf4j
+public class HttpAspect {
+
+    @Pointcut(value = "execution(public * springbootapplication.demo.controller.StudentController.*(..))")
+    public void log() {
+    }
+
+    @Before("log()")
+    public void doBefore(JoinPoint joinPoint) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        log.info("url={}", request.getRequestURL());
+        log.info("method={}", request.getMethod());
+        log.info("ip={}", request.getRemoteAddr());
+        log.info("class method={}", joinPoint.getSignature().getName());
+        log.info("class method={}", joinPoint.getSignature().getDeclaringTypeName());
+        log.info("args={}", joinPoint.getArgs());
+    }
+
+    @AfterReturning(returning = "o", pointcut = "log()")
+    public void doAfterReturning(Object o) {
+        log.info("response={}", o);
+        Result result = (Result) o;
+        Student student = (Student) result.getData();
+        log.info("student:{}", student);
+    }
+
+    @After("log()")
+    public void afterMethod() {
+        log.info("after method is called");
+    }
+}
